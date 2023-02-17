6e9830b3edea9dcbc7588dbe663fefad6f2b2b54
Warren <w.n.bates@gmail.com>
2023-02-17 12:19:41 +0000

    Not create release as part of release action

---
+++ .github/actions/get-release-upload-url/src/index.ts
@@ -0,0 +1,21 @@
+import * as core from "@actions/core";
+import * as github from "@actions/github";
+
+async function run() {
+  try {
+    const repoOptions = github.context.repo;
+    const octokit = github.getOctokit(process.env.GITHUB_TOKEN as string);
+    const tag = github.context.ref
+
+    const release = await octokit.rest.repos.getReleaseByTag({
+        ...repoOptions,
+        tag
+    })
+
+    core.setOutput("upload_url", release.data.upload_url)
+  } catch (error: any) {
+    console.error(error);
+    core.setFailed(error);
+  }
+}
+run();
